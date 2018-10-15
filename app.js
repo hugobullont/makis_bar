78611ce5f7f4c8ef4f50da9793c44381fe80e60d
hugobullont <hugobullont@gmail.com>
2018-10-15 18:48:18 -0500

    Added Database Config

--- app.js
+++ app.js
@@ -16,3 +16,19 @@ require('./routes/index.routes.js')(app);
 app.listen(3000, ()=> {
     console.log('Corriendo en el puerto 3000.');
 });
+
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...');
+    process.exit();
+});
